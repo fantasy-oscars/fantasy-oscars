5856f146ba551af39a172bb9500a87edb5c27704
alexvornsand <alexvornsand@gmail.com>
2026-01-31 07:05:30 -0600

    chore(db): squash migrations into single init file

--- db/migrations/005_league_ceremony_nullable.sql
+++
@@ -1,6 +0,0 @@
--- Allow creating leagues before an active ceremony is configured.
--- This enables a "set up the league first, attach a ceremony later" workflow.
-
-ALTER TABLE public.league
-  ALTER COLUMN ceremony_id DROP NOT NULL;
-
--- db/migrations/014_drop_ceremony_film_candidate.sql
+++
@@ -1,5 +0,0 @@
--- Candidate films are global (film table), not ceremony-scoped.
--- This table was an early iteration; drop it pre-launch.
-
-DROP TABLE IF EXISTS public.ceremony_film_candidate;
-
