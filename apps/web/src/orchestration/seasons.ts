6fde5a4d0405d82f7b552eefff3043137cb3f951
alexvornsand <alexvornsand@gmail.com>
2026-02-11 07:22:46 -0600

    atomize(web): move auth session IO into orchestration

--- apps/web/src/orchestration/seasons.ts
+++ apps/web/src/orchestration/seasons.ts
@@ -2,10 +2,5 @@ export {
   useLeagueSeasonCreateOrchestration,
   type LeagueSeasonCreateView
 } from "./seasons.leagueSeasonCreate";
-export {
-  useSeasonsIndexOrchestration,
-  type SeasonsIndexRow,
-  type SeasonsIndexView
-} from "./seasons.index";
+export { useSeasonsIndexOrchestration, type SeasonsIndexView } from "./seasonsIndex";
 export { useSeasonOrchestration } from "./seasons.season";
-
